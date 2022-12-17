49656c894805dbb97d9ae099d81e51c373546529
a.ranjbar <amin7ranjbar@gmail.com>
2022-12-17 12:11:52 +0330

    change urls

--- bye-world/src/app.controller.ts
+++ bye-world/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('hello')
+@Controller('world')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getBye(): string {
+    return this.appService.getBye();
   }
 }
